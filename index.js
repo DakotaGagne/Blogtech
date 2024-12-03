aa1dec86307033fee95eee3a3d5c8b4596cd6188
Dakota Gagne <dakotaevan10@gmail.com>
2024-12-02 22:46:39 -0700

    Added dark mode toggle

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import exp from "constants";
 
-
 // Constants
 const app = express();
 const port = 3000;
@@ -26,6 +25,12 @@ function generate_uid(){
     return uid;
 }
 
+// Color Mode
+
+let colorMode = "light";
+
+// Prev Post (for navigating back to full_post page)
+let prevPost = "Null"
 
 // Express
 
@@ -35,33 +40,43 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.ejs", { posts: posts });
+  res.render(__dirname + "/views/index.ejs", { posts: posts, colorMode: colorMode });
 });
 
 app.get("/all", (req, res) => {
-    res.render(__dirname + "/views/all_posts.ejs", { posts: posts });
+    res.render(__dirname + "/views/all_posts.ejs", { posts: posts, colorMode: colorMode });
 });
 
 app.get("/tech", (req, res) => {
-    res.render(__dirname + "/views/tech.ejs", { posts: posts });
+    res.render(__dirname + "/views/tech.ejs", { posts: posts, colorMode: colorMode });
 });
 
 app.get("/science", (req, res) => {
-    res.render(__dirname + "/views/science.ejs", { posts: posts });
+    res.render(__dirname + "/views/science.ejs", { posts: posts, colorMode: colorMode });
 });
 
 app.get("/nature", (req, res) => {
-    res.render(__dirname + "/views/nature.ejs", { posts: posts });
+    res.render(__dirname + "/views/nature.ejs", { posts: posts, colorMode: colorMode });
 });
 
 app.get("/create", (req, res) => {
-    res.render(__dirname + "/views/create.ejs", { posts: posts });
+    res.render(__dirname + "/views/create.ejs", { posts: posts, colorMode: colorMode });
+});
+
+app.get("/full_post", (req, res) => {
+    console.log("full_post get called")
+    console.log(JSON.stringify(prevPost))
+    if(prevPost == "Null"){
+      prevPost = posts[posts.length - 1];
+    }
+    res.render(__dirname + "/views/full_post.ejs", { post: prevPost, colorMode: colorMode })
 });
 
 app.post("/full_post", (req, res) => {
     let post = posts.find(post => post.uid == req.body.uid);
-    res.render(__dirname + "/views/full_post.ejs", { post: post });
-});3
+    prevPost = post;
+    res.render(__dirname + "/views/full_post.ejs", { post: post, colorMode: colorMode });
+});
 
 app.post("/create", (req, res) => {
     let newPost = {};
@@ -69,14 +84,51 @@ app.post("/create", (req, res) => {
     newPost.title = format_text(req.body.title);
     newPost.author = format_text(req.body.author);
     newPost.content = req.body.content;
-    newPost.date = new Date().toISOString().split('T')[0]
+    let date = new Date()
+    newPost.date = `${date.toLocaleString('default', { month: 'short' })} ${date.getDate()} ${date.getFullYear()}`
     let imgInt = Math.floor(Math.random() * 3) + 1;
     newPost.image = `./assets/${newPost.category.toLowerCase()}${imgInt}.jpg`;
     newPost.uid = generate_uid();
     console.log("New Post Created!");
     console.log(newPost);
     posts.push(newPost);
-    res.render(__dirname + "/views/create.ejs", { posts: posts, submitted: true });
+    res.render(__dirname + "/views/create.ejs", { posts: posts, submitted: true, colorMode: colorMode });
+});
+
+app.post("/colormode", (req, res) => {
+  colorMode = req.body.colorMode
+  console.log("Color Mode Set to " + req.body.colorMode)
+  
+  switch(req.body.location){
+    case "All":
+      res.redirect("/all");
+      break;
+    case "Tech":
+      res.redirect('/tech');
+      break;
+    case "Nature":
+      res.redirect('/nature');
+      break;
+    case "Science":
+      res.redirect('/science');
+      break;
+    case "Create":
+      res.redirect('/create');
+      break;
+    case "None":
+      if(req.body.postuid){
+        prevPost = posts.find(post => post.uid == req.body.postuid);
+        
+        res.redirect("/full_post");
+      } else {
+        res.redirect("/");
+      }
+      break;
+    default:
+      res.redirect("/");
+      break;
+    
+  }
 });
 
 
@@ -101,7 +153,7 @@ var posts = [
     {
         category: "Science",
         image: "./assets/science1.jpg",
-        date: "2024-01-13",
+        date: "Jan 01 2024",
         title: "Science is Cool!",
         author: "John Doe",
         content: "Science is the systematic study of the structure and behavior of the physical and natural world through observation and experimentation. It has led to countless discoveries and innovations that have transformed our lives unimaginably. From the invention of the wheel to the development of the internet, science has been at the forefront of human progress. It allows us to understand the universe, from the smallest particles to the vastness of space, and everything in between. Science is not just a body of knowledge, but a way of thinking and questioning the world around us. It encourages curiosity, critical thinking, and a desire to explore the unknown. One of the coolest aspects of science is its ability to solve real-world problems. Through scientific research, we have developed life-saving medicines, advanced technologies, and sustainable solutions to environmental challenges. Science has also given us the tools to explore other planets, understand the complexities of the human brain, and even manipulate the building blocks of life through genetic engineering. The possibilities are endless, and the pursuit of scientific knowledge continues to push the boundaries of what we know and can achieve. Science is a testament to human ingenuity and our relentless quest for understanding and improvement.",
@@ -110,7 +162,7 @@ var posts = [
     {
         category: "Tech",
         image: "./assets/tech1.jpg",
-        date: "2024-01-22",
+        date: "Jan 22 2024",
         title: "Tech is Cool!",
         author: "Jane Doe",
         content: "Technology is an ever-evolving field that has revolutionized the way we live, work, and interact with the world around us. From the invention of the wheel to the development of the internet, technology has been a driving force behind human progress. It has enabled us to achieve feats that were once thought impossible, such as instant global communication, space exploration, and advanced medical treatments. The rapid pace of technological advancements continues to push the boundaries of what we can accomplish, making our lives more convenient, efficient, and connected. One of the coolest aspects of technology is its ability to bring people together and bridge gaps across distances. Social media platforms, video conferencing tools, and messaging apps have made it easier than ever to stay in touch with friends and family, no matter where they are in the world. Additionally, technology has democratized access to information and education, allowing people from all walks of life to learn new skills, pursue their passions, and contribute to the global knowledge economy. As we look to the future, the potential for technology to drive positive change and improve our quality of life is limitless, making it an exciting and dynamic field to be a part of.",
@@ -119,7 +171,7 @@ var posts = [
     {
         category: "Nature",
         image: "./assets/nature1.jpg",
-        date: "2024-02-03",
+        date: "Feb 03 2024",
         title: "Nature is Cool!",
         author: "John Doe",
         content: "Nature is the natural world that surrounds us, encompassing all living and non-living things that exist independently of human activities. It includes the flora and fauna, landscapes, oceans, mountains, and all the ecosystems that make up our planet. Nature is a source of inspiration, beauty, and tranquility, providing us with a sense of peace and connection to the world around us. It plays a crucial role in sustaining life, offering resources such as clean air, water, and food, as well as regulating the climate and supporting biodiversity. The beauty of nature can be seen in the vibrant colors of a sunset, the intricate patterns of a leaf, and the majestic presence of a mountain range. It reminds us of the delicate balance that exists in the natural world and the importance of preserving it for future generations. By appreciating and protecting nature, we can ensure that its wonders continue to inspire and sustain us for years to come.",
@@ -128,7 +180,7 @@ var posts = [
     {
         category: "Science",
         image: "./assets/science2.jpg",
-        date: "2024-02-18",
+        date: "Feb 18 2024",
         title: "Cool Science Experiments!",
         author: "Jane Doe",
         content: "One cool science experiment that has captivated the imagination of many is the classic baking soda and vinegar volcano. This simple yet fascinating experiment demonstrates the chemical reaction between an acid (vinegar) and a base (baking soda), resulting in the production of carbon dioxide gas. When these two substances are combined, they create a bubbly, frothy eruption that mimics the appearance of a volcanic explosion. This experiment is not only visually impressive but also serves as an excellent introduction to basic chemistry concepts for students and enthusiasts alike. It highlights the principles of chemical reactions, gas production, and the effects of combining different substances. Another intriguing science experiment is the creation of a homemade lava lamp. By using common household items such as vegetable oil, water, food coloring, and effervescent tablets, you can create a mesmerizing display of colorful blobs that float and sink in a liquid-filled container. This experiment demonstrates the concepts of density and immiscibility, as the oil and water do not mix and have different densities. The effervescent tablets release carbon dioxide gas, causing the colored water to rise and fall in the oil, creating a captivating visual effect. This experiment not only provides a fun and engaging way to explore scientific principles but also results in a decorative and entertaining piece that can be enjoyed by people of all ages.",
@@ -137,7 +189,7 @@ var posts = [
     {
         category: "Tech",
         image: "./assets/tech2.jpg",
-        date: "2024-03-10",
+        date: "Mar 10 2024",
         title: "Cool Tech Inventions!",
         author: "John Doe",
         content: "Imagine a device that can instantly translate spoken language in real-time, allowing people from different parts of the world to communicate effortlessly. This hypothetical tech invention, called the Universal Translator, would be a small, portable gadget that you can carry in your pocket. Equipped with advanced speech recognition and artificial intelligence, the Universal Translator would be able to understand and translate any language with near-perfect accuracy. It would not only translate words but also capture the nuances and emotions behind them, making conversations more meaningful and authentic. This invention would revolutionize global communication, breaking down language barriers and fostering greater understanding and collaboration among people from diverse cultures. Another exciting tech invention could be the HoloPhone, a smartphone that projects holographic images and videos in 3D. The HoloPhone would use cutting-edge holographic display technology to create lifelike, interactive holograms that can be viewed from any angle. Imagine being able to have a video call where the person you're talking to appears as a hologram right in front of you, or watching a movie where the characters seem to come to life in your living room. The HoloPhone would also have advanced gesture recognition, allowing you to interact with the holograms using hand movements. This invention would take communication and entertainment to a whole new level, providing an immersive and engaging experience that goes beyond the capabilities of current smartphones.",
@@ -146,7 +198,7 @@ var posts = [
     {
         category: "Nature",
         image: "./assets/nature2.jpg",
-        date: "2024-03-27",
+        date: "Mar 27 2024",
         title: "The Beauty of Forests",
         author: "Jane Doe",
         content: "Forests are one of the most diverse and vital ecosystems on our planet. They provide habitat for countless species of plants and animals, regulate the climate, and produce oxygen. Walking through a forest can be a serene and rejuvenating experience, surrounded by the sounds of birds and the rustling of leaves. Protecting our forests is essential for maintaining biodiversity and combating climate change.",
@@ -155,7 +207,7 @@ var posts = [
     {
         category: "Science",
         image: "./assets/science3.jpg",
-        date: "2024-04-02",
+        date: "Apr 02 2024",
         title: "Exploring Space",
         author: "John Doe",
         content: "Space exploration has always fascinated humanity. From the first moon landing to the latest Mars rover missions, our quest to understand the universe continues. Advances in technology are enabling us to explore further and learn more about our solar system and beyond. Space exploration not only satisfies our curiosity but also drives innovation and inspires future generations.",
@@ -164,7 +216,7 @@ var posts = [
     {
         category: "Tech",
         image: "./assets/tech2.jpg",
-        date: "2024-04-13",
+        date: "Apr 13 2024",
         title: "The Future of AI",
         author: "Jane Doe",
         content: "Artificial Intelligence (AI) is transforming industries and changing the way we live and work. From self-driving cars to advanced medical diagnostics, AI has the potential to revolutionize many aspects of our lives. As AI technology continues to advance, it will bring new opportunities and challenges, making it an exciting field to watch.",
@@ -173,7 +225,7 @@ var posts = [
     {
         category: "Nature",
         image: "./assets/nature3.jpg",
-        date: "2024-04-20",
+        date: "Apr 20 2024",
         title: "Ocean Wonders",
         author: "John Doe",
         content: "The ocean covers more than 70% of the Earth's surface and is home to an incredible array of marine life. From the smallest plankton to the largest whales, the ocean is teeming with life. It also plays a crucial role in regulating the Earth's climate and weather patterns. Exploring the ocean's depths reveals a world of mystery and beauty, with vibrant coral reefs, deep-sea creatures, and underwater landscapes.",
@@ -182,7 +234,7 @@ var posts = [
     {
         category: "Science",
         image: "./assets/science1.jpg",
-        date: "2024-05-11",
+        date: "May 11 2024",
         title: "The Human Genome",
         author: "Jane Doe",
         content: "The study of the human genome has revolutionized our understanding of genetics and medicine. By mapping the entire human genome, scientists have gained insights into the genetic basis of diseases and developed new treatments. This knowledge is paving the way for personalized medicine, where treatments can be tailored to an individual's genetic makeup, improving outcomes and reducing side effects.",
@@ -191,7 +243,7 @@ var posts = [
     {
         category: "Tech",
         image: "./assets/tech1.jpg",
-        date: "2024-05-23",
+        date: "May 23 2024",
         title: "Smart Homes",
         author: "John Doe",
         content: "Smart home technology is making our lives more convenient and efficient. From voice-activated assistants to automated lighting and security systems, smart home devices are becoming increasingly popular. These innovations allow us to control our homes with ease, improve energy efficiency, and enhance security, making our living spaces more comfortable and connected.",
@@ -200,7 +252,7 @@ var posts = [
     {
         category: "Nature",
         image: "./assets/nature1.jpg",
-        date: "2024-06-13",
+        date: "Jun 13 2024",
         title: "Mountain Majesty",
         author: "Jane Doe",
         content: "Mountains are some of the most awe-inspiring natural features on Earth. They offer breathtaking views, challenging hikes, and a sense of tranquility. Mountains are also important for their role in the water cycle, as they capture and store precipitation, providing fresh water to rivers and streams. The diverse ecosystems found in mountainous regions are home to unique flora and fauna.",
@@ -209,7 +261,7 @@ var posts = [
     {
         category: "Science",
         image: "./assets/science2.jpg",
-        date: "2024-07-18",
+        date: "Jul 18 2024",
         title: "Climate Change",
         author: "John Doe",
         content: "Climate change is one of the most pressing issues of our time. Scientific research is crucial for understanding the causes and impacts of climate change and developing strategies to mitigate its effects. From studying ice cores to monitoring atmospheric changes, scientists are working tirelessly to provide the data and insights needed to address this global challenge and protect our planet for future generations.",
@@ -218,7 +270,7 @@ var posts = [
     {
         category: "Tech",
         image: "./assets/tech1.jpg",
-        date: "2024-08-28",
+        date: "Aug 28 2024",
         title: "Wearable Tech",
         author: "Jane Doe",
         content: "Wearable technology, such as smartwatches and fitness trackers, is becoming an integral part of our daily lives. These devices help us monitor our health, stay connected, and manage our time more effectively. As wearable tech continues to evolve, it will offer even more advanced features and capabilities, making it an essential tool for personal and professional use.",
@@ -227,7 +279,7 @@ var posts = [
     {
         category: "Nature",
         image: "./assets/nature2.jpg",
-        date: "2024-09-15",
+        date: "Sept 15 2024",
         title: "Desert Landscapes",
         author: "John Doe",
         content: "Deserts may seem barren, but they are full of life and beauty. These arid landscapes are home to specially adapted plants and animals that thrive in harsh conditions. The stark beauty of deserts, with their vast sand dunes, rocky outcrops, and clear night skies, offers a unique and peaceful experience. Deserts also hold important clues to the Earth's geological history and climate.",
